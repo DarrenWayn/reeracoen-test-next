c934637628c5e4e2f32b8137e7e379eb48b77aae
Darren Wayn <darrenwayn98@gmail.com>
2022-09-27 11:51:27 +0700

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    useTabs: false,
+    tabWidth: 2,
+    printWidth: 100,
+    arrowParens: "always",
+    trailingComma: "all",
+  };
